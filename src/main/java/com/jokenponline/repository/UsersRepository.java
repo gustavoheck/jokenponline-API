4a94b7e700966c00d833f777c4224a29b7c760be
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-02-09 18:36:36 -0300

    Changing users repository type

--- src/main/java/com/jokenponline/repository/UsersRepository.java
+++ src/main/java/com/jokenponline/repository/UsersRepository.java
@@ -1,9 +1,10 @@
 package com.jokenponline.repository;
 
+import com.jokenponline.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<UsersRepository, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
